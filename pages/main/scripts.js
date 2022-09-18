0addf719bed05791e220ad2032e1a12d6a3d9afa
Andrey <spacecmetanka@gmail.com>
2022-09-19 00:19:26 +0300

    feat : submit btn validation

--- pages/main/scripts.js
+++ pages/main/scripts.js
@@ -1,12 +1,20 @@
 const formInput = document.querySelector('.footer__form-email__input')
 const label = document.querySelector('.footer__form-email__label');
+const form = document.querySelector('.footer__form')
 formInput.addEventListener('click', (e) => {
     label.classList.remove('footer__form-email__label-inactive')
     label.classList.add('footer__form-email__label-focused')
     label.style.position = 'relative'
 })
 
-formInput.addEventListener('blur' , ()=> {
+formInput.addEventListener('blur', () => {
     label.classList.add('footer__form-email__label-inactive')
     label.classList.remove('footer__form-email__label-focused')
+})
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const btn = document.querySelector('.footer__form-btn');
+    btn.classList.add('footer__form-btn-default')
+    btn.classList.remove('footer__form-btn')
 })
